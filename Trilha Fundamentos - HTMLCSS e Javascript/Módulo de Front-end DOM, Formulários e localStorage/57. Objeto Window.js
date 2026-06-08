637b1b8c474d4d2971f16104452116fd9e277f3d
gparrini <gpparrinijr@gmail.com>
2026-06-07 21:00:52 -0300

    organização estudos

---
+++ Trilha Fundamentos - HTMLCSS e Javascript/Módulo de Front-end DOM, Formulários e localStorage/57. Objeto Window.js
@@ -0,0 +1,7 @@
+//Utilizar dentro do Inspecionar - DevTools
+console.log(window.document);
+console.log(window.location);
+console.log(window.location.href);
+console.log(window.screen.width);
+console.log(window.innerWidth);
+window.document.title = "Millene"; // o title da página irá alterar localmente
